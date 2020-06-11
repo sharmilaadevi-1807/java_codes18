867c66b4728cee41367755f52eb236802e4a14e7
sharmilaadevi-1807 <65936202+sharmilaadevi-1807@users.noreply.github.com>
2020-06-11 09:32:13 +0530

    Add files via upload

---
+++ alphabetornot.java
@@ -0,0 +1,15 @@
+
+public class alphabetornot {
+
+
+
+    public static void main(String[] args) {
+
+        char c = '*';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
