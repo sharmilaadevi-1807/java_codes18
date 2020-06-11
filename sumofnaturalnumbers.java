e32f335023b693ab7a9746e16386259f86b753cc
sharmilaadevi-1807 <65936202+sharmilaadevi-1807@users.noreply.github.com>
2020-06-11 09:27:32 +0530

    Add files via upload

---
+++ sumofnaturalnumbers.java
@@ -0,0 +1,18 @@
+
+public class sumofnaturalnumbers {
+
+
+
+    public static void main(String[] args) {
+
+        int num = 100, sum = 0;
+
+        for(int i = 1; i <= num; ++i)
+        {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
