4f516161e9d56a06a4c7fd81ce8670d956e5ae44
sharmilaadevi-1807 <65936202+sharmilaadevi-1807@users.noreply.github.com>
2020-06-11 09:48:55 +0530

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,17 @@
+package github;
+
+public class ASCII {
+
+
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
